c9189dcfa408646560d00b948020b71429add57f
Karlos Alvan <karlos.alvan@gmail.com>
2018-05-12 13:26:32 +0200

    ADD react three startup

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { combineReducers } from 'redux'
+
+const app = (state = {}, action) => {
+  switch (action.type) {
+    
+    default:
+      return state;
+  }
+}
+
+const Reducer = combineReducers({ app })
+
+export default Reducer
